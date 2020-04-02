c71711f6577d9de64911a38dcabf931dae3e5e25
bannednickyname <liuyang-ace@qq.com>
2020-04-02 14:06:19 +0800

    SpringBoot+JWT+SpringSecurity登录权鉴

---
+++ 07-codingstudy-login/src/main/java/com/codingstudy/login/service/auth/AuthUserDetailsServiceImpl.java
@@ -0,0 +1,53 @@
+package com.codingstudy.login.service.auth;
+
+import com.codingstudy.login.entity.SysRoleTable;
+import com.codingstudy.login.entity.SysUserEntity;
+import com.codingstudy.login.service.SysRoleTableService;
+import com.codingstudy.login.service.SysUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author K. L. Mao
+ * @create 2019/1/11
+ */
+@Service
+public class AuthUserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private SysUserService userService;
+
+    @Autowired
+    private SysRoleTableService roleService;
+
+    /**
+     * 通过账号查找用户、角色的信息
+     * @param username
+     * @return
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        SysUserEntity user = userService.getUserByUserName(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(String.format("%s.这个用户不存在", username));
+        }else {
+            //查找角色
+            List<String> roles =  roleService.getRolesByUserName(username);
+            List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+            for (String role : roles) {
+                authorities.add(new SimpleGrantedAuthority(role));
+            }
+            System.out.println("loadUserByUsername......user ===> " + user);
+            return new AuthUser(user.getUserName(), user.getPassWord(), user.getState(), authorities);
+        }
+    }
+}
